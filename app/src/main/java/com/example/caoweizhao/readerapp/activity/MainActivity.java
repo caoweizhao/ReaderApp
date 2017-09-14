c515223aac23cdfe52ec2f0a68ba95593c747388
caoweizhao <caoweizhao0706>
2017-09-14 21:25:26 +0800

    aaa

---
+++ app/src/main/java/com/example/caoweizhao/readerapp/activity/MainActivity.java
@@ -0,0 +1,10 @@
+package com.example.caoweizhao.readerapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by caoweizhao on 2017-9-14.
+ */
+
+public class MainActivity extends AppCompatActivity {
+}
